ab8dc3eca951c1d5e1a5ff35528f239f7aa3dbdc
DruideRappeur <hyperdrift.68@gmail.com>
2013-11-04 17:12:31 +0100

    Added OctogonCell and SquareCell subclasses Numbers for energy transfer

--- main/prototype/model.py
+++ main/prototype/model.py
@@ -5,6 +5,7 @@ Created on 4 nov. 2013
 '''
 
 class Cell:
+    celltype=""
     def __init__(self, x, y, owner):
         self.x=x
         self.y=y
@@ -34,7 +35,40 @@ class Cell:
                 
     def transfer_amount(self, target):
         pass
-    
+
+class OctogonCell(Cell):
+    celltype="octogon"
+    def generate_energy(self):
+        self.energy+=0.025
+    def transfer_amount(self, target):
+        nconn=len(self.targets)
+        if nconn==1:
+            if target.celltype=="octogon":
+                return self.energy*.0081 # .81% of en /tick <=> 15% per second
+            else:
+                return self.energy*.0053 # .53% of en/tick <=> 10% per second
+        elif nconn==2:
+            if target.celltype=="octogon":
+                return self.energy*.0053 # .53% of en/tick <=> 10% per second
+            else:
+                return self.energy*.0039 # .39% of en/tick <=> 7.5% per second
+        elif nconn==3:
+            if target.celltype=="octogon":
+                return self.energy*.0043 # .43% of en/tick <=> 8.33% per second
+            else:
+                return self.energy*.0034 # .34% of en/tick <=> 6.67% per second
+            
+class SquareCell(Cell):
+    celltype="square"
+    def generate_energy(self):
+        self.energy+=0.1
+    def transfer_amount(self, target):
+        if target.celltype=="octogon":
+            return self.energy*.014 # 1.4% of en/tick <=> 25% per second
+        else:
+            return self.energy*.011 # 1.1% of en/tick <=> 20% per second
+            
+                
     
             
             
