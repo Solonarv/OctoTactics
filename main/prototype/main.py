dec614bbb8330344839ec04ec4fac700e25ee8d5
DruideRappeur <hyperdrift.68@gmail.com>
2013-11-04 17:40:24 +0100

    Added basic UI stuff

---
+++ main/prototype/main.py
@@ -0,0 +1,11 @@
+'''
+Created on 4 nov. 2013
+
+@author: alex
+'''
+from tkinter import *
+
+window=Tk()
+window.title("OctoTactics - Prototype")
+window.geometry("640x480")
+canvas=Canvas(window, bg="black", )
