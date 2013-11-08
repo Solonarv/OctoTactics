5da1aaf6c6a03e0b03b5cdd381e2cbe365dc9efa
DruideRappeur <hyperdrift.68@gmail.com>
2013-11-08 18:45:09 +0100

    Added main buttons and structures for menu

--- main/prototype/main.py
+++ main/prototype/main.py
@@ -5,7 +5,23 @@ Created on 4 nov. 2013
 '''
 from tkinter import *
 
+def startgame():
+    pass
+
+def settings():
+    pass
+
+def leavegame():
+    import sys
+    sys.exit(0)
+
 window=Tk()
 window.title("OctoTactics - Prototype")
 window.geometry("640x480")
 canvas=Canvas(window, bg="black", )
+
+presentation=Label(window, text="Welcome on our first game ever: OctoTactics !")
+
+launchgame=Button(window, text="Play!", command=startgame)
+settings=Button(window, text="Settings", command=settings)
+quitgame=Button(window, text="Quit game - Bad idea!", command=leavegame)
