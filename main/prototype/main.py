e1f5c23a3ecbb8b12cdbbde112ef9d1bd8ad37dd
DruideRappeur <hyperdrift.68@gmail.com>
2013-11-08 19:40:58 +0100

    Added button positioning for prototype menu

--- main/prototype/main.py
+++ main/prototype/main.py
@@ -3,7 +3,9 @@ Created on 4 nov. 2013
 
 @author: alex
 '''
+
 from tkinter import *
+import sys
 
 def startgame():
     pass
@@ -12,7 +14,6 @@ def settings():
     pass
 
 def leavegame():
-    import sys
     sys.exit(0)
 
 window=Tk()
@@ -23,5 +24,8 @@ canvas=Canvas(window, bg="black", )
 presentation=Label(window, text="Welcome on our first game ever: OctoTactics !")
 
 launchgame=Button(window, text="Play!", command=startgame)
+launchgame.grid(row=1,column=1)
 settings=Button(window, text="Settings", command=settings)
+settings.grid(row=2,column=1)
 quitgame=Button(window, text="Quit game - Bad idea!", command=leavegame)
+quitgame.grid(row=3,column=1)
