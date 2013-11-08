c4a6a99e58ce29efda7c36ebae85d6d31c9ccc47
Solonarv <nstamm@gmx.de>
2013-11-08 19:00:02 +0000

    /Finally / fixed main.py

--- main/prototype/main.py
+++ main/prototype/main.py
@@ -5,7 +5,7 @@ Created on 4 nov. 2013
 '''
 
 from tkinter import *
-import sys
+from sys import exit
 
 def startgame():
     pass
@@ -14,7 +14,7 @@ def settings():
     pass
 
 def leavegame():
-    sys.exit(0)
+    exit(0)
 
 window=Tk()
 window.title("OctoTactics - Prototype")
@@ -22,10 +22,8 @@ window.geometry("640x480")
 canvas=Canvas(window, bg="black", )
 
 presentation=Label(window, text="Welcome on our first game ever: OctoTactics !")
+launchgame=Button(window, text="Play!", command=startgame); launchgame.grid(column=1, row=1)
+settings=Button(window, text="Settings", command=settings); launchgame.grid(column=2, row=1)
+quitgame=Button(window, text="Quit game - Bad idea!", command=leavegame); launchgame.grid(column=3, row=1)
 
-launchgame=Button(window, text="Play!", command=startgame)
-launchgame.grid(row=1,column=1)
-settings=Button(window, text="Settings", command=settings)
-settings.grid(row=2,column=1)
-quitgame=Button(window, text="Quit game - Bad idea!", command=leavegame)
-quitgame.grid(row=3,column=1)
+window.mainloop()
