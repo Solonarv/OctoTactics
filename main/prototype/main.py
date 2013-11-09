f406f5330ccabdf9e518a2648ceaa706558e6473
DruideRappeur <hyperdrift.68@gmail.com>
2013-11-09 17:18:57 +0100

    Fixed menu bugs

--- main/prototype/main.py
+++ main/prototype/main.py
@@ -22,8 +22,11 @@ window.geometry("640x480")
 canvas=Canvas(window, bg="black", )
 
 presentation=Label(window, text="Welcome on our first game ever: OctoTactics !")
-launchgame=Button(window, text="Play!", command=startgame); launchgame.grid(column=1, row=1)
-settings=Button(window, text="Settings", command=settings); launchgame.grid(column=2, row=1)
-quitgame=Button(window, text="Quit game - Bad idea!", command=leavegame); launchgame.grid(column=3, row=1)
+launchgame=Button(window, text="Play!", command=startgame); 
+launchgame.grid(column=1, row=1)
+settings=Button(window, text="Settings", command=settings); 
+settings.grid(column=1, row=2)
+quitgame=Button(window, text="Quit game - Bad idea!", command=leavegame); 
+quitgame.grid(column=1, row=3)
 
 window.mainloop()
