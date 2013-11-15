55d119e1811f546eb30a1bc088eab22b96c839a2
Solonarv <nstamm@gmx.de>
2013-11-15 11:32:47 +0100

    Removed semicolons

--- main/prototype/main.py
+++ main/prototype/main.py
@@ -22,11 +22,11 @@ window.geometry("640x480")
 canvas=Canvas(window, bg="black", )
 
 presentation=Label(window, text="Welcome on our first game ever: OctoTactics !")
-launchgame=Button(window, text="Play!", command=startgame); 
+launchgame=Button(window, text="Play!", command=startgame)
 launchgame.grid(column=1, row=1)
-settings=Button(window, text="Settings", command=settings); 
+settings=Button(window, text="Settings", command=settings)
 settings.grid(column=1, row=2)
-quitgame=Button(window, text="Quit game - Bad idea!", command=leavegame); 
+quitgame=Button(window, text="Quit game - Bad idea!", command=leavegame)
 quitgame.grid(column=1, row=3)
 
 window.mainloop()
