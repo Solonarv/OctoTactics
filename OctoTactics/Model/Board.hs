3f008105782300aa97ffc6a944e37c6496f32b69
Solonarv <nstamm@gmx.de>
2016-01-15 19:45:06 +0100

    Trying to do things with lenses

--- OctoTactics/Model/Board.hs
+++ OctoTactics/Model/Board.hs
@@ -12,6 +12,8 @@ import qualified Data.Array as Array
 import Data.Set (Set)
 import qualified Data.Set as Set
 
+import Control.Lens
+
 import OctoTactics.Util.ImprovedPrelude
 import OctoTactics.Model.Cell
 import OctoTactics.Model.Player
@@ -29,24 +31,22 @@ tick = addEnergy . drainCells . generateEnergy
 
 generateEnergy :: Board Cell
                -> Board Cell
-generateEnergy = fmap $ second $ \c -> c { cEnergy = cEnergy c + regenRate (cType c) }
+generateEnergy = fmap $ second $ \c -> (cEnergy +~ c^.regenRate) c
 
 drainCells :: Board Cell
            -> Board (Cell, Set (Position, Double))
-drainCells b = b <$$> \(pos, c) -> let targets    = cConnections c
-                                       ccount     = length (cConnections c)
-                                       energy     = cEnergy c
-                                       baseAmount = energy * transferRates (cType c) ! ccount
-                                       transfers  = targets <$$> \tpos -> let tc = snd $ b ! tpos
-                                                                              modifier = receptionModifier (cType tc) * (if cSide tc `allied` cSide c then 1 else -1)
-                                                                          in (tpos, baseAmount * modifier)
-                                       next       = c { cEnergy = energy - sum (snd <$> transfers) }
+drainCells b = b <$$> \(pos, c) -> let ccount     = c^.cConnections.to length
+                                       baseAmount = energy * c^.transferRates ! ccount
+                                       transfers  = c & cConnections %~ fmap (\tpos -> let tc = snd $ b ! tpos
+                                                                                           modifier = tc^.receptionModifier * (if tc^.cSide `allied` c^.cSide then 1 else -1)
+                                                                                       in (tpos, baseAmount * modifier))
+                                       next       = c & cEnergy -~ sum (snd <$> transfers)
                                    in (pos, (next, transfers))
 
 addEnergy :: Board (Cell, Set (Position, Double))
           -> Board Cell
-addEnergy b = b <$$> \(pos, (c, _)) -> let deltaE = sum $  b <$$> snd $>>= \(c', ts) -> ts <? ((== pos) . fst) <$$> snd
-                                       in (pos, c { cEnergy = cEnergy c + deltaE })
+addEnergy b = b <&> \(pos, (c, _)) -> let deltaE = b <&> snd $>>= (\(c', ts) -> ts <? ((== pos) . fst) <&> snd) & sum
+                                      in (pos, c & cEnergy +~ deltaE )
 
 cellAt :: Position -> Board a -> a
 cellAt p = snd . (! p)
--- OctoTactics/Model/Cell.hs
+++ OctoTactics/Model/Cell.hs
@@ -1,13 +1,16 @@
 {-# LANGUAGE
     ExplicitNamespaces,
-    NoImplicitPrelude
+    NoImplicitPrelude,
+    TemplateHaskell
     #-}
 
 module OctoTactics.Model.Cell (
-    type Cell(..), 
+    type Cell(..),
+    HasCell(..),
+    HasCellType(..),
     CellType(..),
     newCell,
-    cell
+    mkCell
     ) where
 
 import Data.Set (Set)
@@ -16,71 +19,78 @@ import qualified Data.Set as Set
 import Data.Array (Array)
 import qualified Data.Array as Array
 
+import Control.Lens hiding (both)
+
 import OctoTactics.Data.Direction
 import OctoTactics.Model.Player (Player)
 import OctoTactics.Render.CellRenderer (CellRenderer, renderOctogon, renderSquare)
 import OctoTactics.Util.ImprovedPrelude
 import OctoTactics.Util.Conversion (intToDouble)
 
--- | The cell type itself
-data Cell = Cell { cPosition    :: !(Int, Int)
-                 , cType        :: !CellType
-                 , cEnergy      :: !Double
-                 , cConnections :: !(Set (Int, Int))
-                 , cSide        :: !(Maybe Player)
-                 }
-
--- | Stores information about cell types.
-data CellType = CellType { legalDirections   :: !(Set Direction)
-                         , name              :: !String
-                         , maxTargets        :: !Int
-                         , startingEnergy    :: !Double
-                         , regenRate         :: !Double
-                         , transferRates     :: !(Array Int Double)
-                         , receptionModifier :: !Double
-                         , renderer          :: !CellRenderer
-                         , rangeSquared      :: !Double
+                 
+-- | Stores information about _cell types.
+data CellType = CellType { _legalDirections   :: !(Set Direction)
+                         , _name              :: !String
+                         , _maxTargets        :: !Int
+                         , _startingEnergy    :: !Double
+                         , _regenRate         :: !Double
+                         , _transferRates     :: !(Array Int Double)
+                         , _receptionModifier :: !Double
+                         , _renderer          :: !CellRenderer
+                         , _rangeSquared      :: !Double
                          }
 
-instance Eq CellType where
-    (==) = (==) `on` name
+makeClassy ''CellType
 
-octogon = CellType { legalDirections   = Set.fromList [North .. NorthWest]
-                   , name              = "octogon"
-                   , maxTargets        = 3
-                   , startingEnergy    = 5
-                   , regenRate         = 3.75
-                   , transferRates     = Array.listArray (0, 3) [0, 0.216, 0.146, 0.122]
-                   , receptionModifier = 1
-                   , renderer          = renderOctogon
-                   , rangeSquared      = 1.1
+-- | The _cell type itself
+data Cell = Cell { _cType        :: !CellType
+                 , _cEnergy      :: !Double
+                 , _cConnections :: !(Set (Int, Int))
+                 , _cSide        :: !(Maybe Player)
+                 }
+
+makeClassy ''Cell
+
+instance HasCellType Cell where
+    cellType = lens _cType $ \c t -> (cType.~t) c
+
+instance Eq CellType where
+    (==) = (==) `on` _name
+
+octogon = CellType { _legalDirections   = Set.fromList [North .. NorthWest]
+                   , _name              = "octogon"
+                   , _maxTargets        = 3
+                   , _startingEnergy    = 5
+                   , _regenRate         = 3.75
+                   , _transferRates     = Array.listArray (0, 3) [0, 0.216, 0.146, 0.122]
+                   , _receptionModifier = 1
+                   , _renderer          = renderOctogon
+                   , _rangeSquared      = 1.1
                    }
 
-square  = CellType { legalDirections   = Set.fromList [North, East, South, West]
-                   , name              = "square"
-                   , maxTargets        = 1
-                   , startingEnergy    = 5
-                   , regenRate         = 3
-                   , transferRates     = Array.listArray (0, 1) [0, 0.35]
-                   , receptionModifier = 0.8
-                   , renderer          = renderSquare
-                   , rangeSquared      = 2.2
+square  = CellType { _legalDirections   = Set.fromList [North, East, South, West]
+                   , _name              = "square"
+                   , _maxTargets        = 1
+                   , _startingEnergy    = 5
+                   , _regenRate         = 3
+                   , _transferRates     = Array.listArray (0, 1) [0, 0.35]
+                   , _receptionModifier = 0.8
+                   , _renderer          = renderSquare
+                   , _rangeSquared      = 2.2
                    }
 
 -- | Create a new cell, with no connections.
-newCell :: (Int, Int)   -- ^ The cell's position
-        -> CellType     -- ^ The cell's type
+newCell :: CellType     -- ^ The cell's type
         -> Maybe Player -- ^ The cell's owner
         -> Cell
-newCell pos typ = Cell pos typ (startingEnergy typ) Set.empty
+newCell typ = Cell typ (typ^.startingEnergy) Set.empty
 
--- | Smart constructor for Cell
-cell :: (Int, Int)     -- ^ The cell's position
-     -> CellType       -- ^ The cell's type
-     -> Double         -- ^ The cell's energy
-     -> Set (Int, Int) -- ^ The locations of the cells this cell is connected to
-     -> Maybe Player   -- ^ The cell's owner
-     -> Maybe Cell
-cell pos typ energy connections owner = if length connections <= maxTargets typ && all ((<= rangeSquared typ) . intToDouble . uncurry (*) . both abs. both2 (-) pos) connections
-                                        then Just (Cell pos typ energy connections owner)
-                                        else Nothing
+-- | Smart _constru_ctor for Cell
+mkCell :: CellType       -- ^ The cell's type
+       -> Double         -- ^ The cell's energy
+       -> Set (Int, Int) -- ^ The locations of the cells this cell is connected to
+       -> Maybe Player   -- ^ The cell's owner
+       -> Maybe Cell
+mkCell typ energy connections owner = if length connections <= typ^.maxTargets -- && all ((<= (typ^.rangeSquared)) . intToDouble . uncurry (*) . both abs. both2 (-) pos) connections
+                                      then Just (Cell typ energy connections owner)
+                                      else Nothing
